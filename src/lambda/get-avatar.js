779388086898e292156ea377cd48c27a45e2a3e9
Phil Hawksworth <phil@netify.com>
2018-03-25 15:32:24 -0500

    tidy up the lambda

--- src/lambda/get-avatar.js
+++ src/lambda/get-avatar.js
@@ -5,21 +5,22 @@ const request = require('request');
 
 export function handler(event, context, callback) {
 
-
+  // grab the parameters from the request
   var size = event.queryStringParameters['size'];
   var user = event.queryStringParameters['user'];
+
+  // transform the size parameter into the image URL pattern needed in the request
   var option = {
     "small" : "_normal",
     "medium" : "_96",
     "large" : "_400x400"
   };
 
-  console.log(`Fetch avatart URL for ${user} with these size parameters of ${option[size]}`);
+  // Report back to our logs
+  console.log(`Fetch avatar URL for ${user} with these size parameters of ${option[size]}`);
 
-  request('https://mobile.twitter.com/' + event.queryStringParameters['user'], function(err, response, body){
-
-
-    // format the response to be a bit mor concise and return it to the client
+  // Make the request to twitter and parse the response
+  request('https://mobile.twitter.com/' + user, function(err, response, body){
     if(!err && response.statusCode === 200){
       const $ = cheerio.load(body);
       const avatar = ($('.avatar img').attr('src') || '').replace('_normal', option[size]);
@@ -35,5 +36,4 @@ export function handler(event, context, callback) {
         })
       }
     })
-
 }
