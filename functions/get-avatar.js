4b2026afb9affed3d60a42ea826d0cd700219ebe
Phil Hawksworth <phil@netify.com>
2018-03-25 13:23:57 -0500

    build size arguments

--- functions/get-avatar.js
+++ functions/get-avatar.js
@@ -30177,12 +30177,22 @@ var request = __webpack_require__(399);
 
 function handler(event, context, callback) {
 
+  var size = event.queryStringParameters['size'];
+  var user = event.queryStringParameters['user'];
+  var option = {
+    "small": "_normal",
+    "medium": "_96",
+    "large": "_400x400"
+  };
+
+  console.log('Fetch avatart URL for ' + user + ' with these size parameters of ' + option[size]);
+
   request('https://mobile.twitter.com/' + event.queryStringParameters['user'], function (err, response, body) {
 
     // format the response to be a bit mor concise and return it to the client
     if (!err && response.statusCode === 200) {
       var $ = cheerio.load(body);
-      var avatar = ($('.avatar img').attr('src') || '').replace('_normal', '_400x400');
+      var avatar = ($('.avatar img').attr('src') || '').replace('_normal', option[size]);
       return callback(null, {
         statusCode: 200,
         headers: { "Content-Type": "application/json" },
--- src/lambda/get-avatar.js
+++ src/lambda/get-avatar.js
@@ -5,12 +5,24 @@ const request = require('request');
 
 export function handler(event, context, callback) {
 
+
+  var size = event.queryStringParameters['size'];
+  var user = event.queryStringParameters['user'];
+  var option = {
+    "small" : "_normal",
+    "medium" : "_96",
+    "large" : "_400x400"
+  };
+
+  console.log(`Fetch avatart URL for ${user} with these size parameters of ${option[size]}`);
+
   request('https://mobile.twitter.com/' + event.queryStringParameters['user'], function(err, response, body){
+
 
     // format the response to be a bit mor concise and return it to the client
     if(!err && response.statusCode === 200){
       const $ = cheerio.load(body);
-      const avatar = ($('.avatar img').attr('src') || '').replace('_normal', '_400x400');
+      const avatar = ($('.avatar img').attr('src') || '').replace('_normal', option[size]);
       return callback(null, {
           statusCode: 200,
           headers: {"Content-Type": "application/json"},
