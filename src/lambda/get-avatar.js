cbae9707fb9963c0aab1c0f959390a1812755bc8
Phil Hawksworth <phil@netify.com>
2018-03-25 12:25:30 -0500

    testing without wildcards

--- src/lambda/get-avatar.js
+++ src/lambda/get-avatar.js
@@ -7,15 +7,14 @@ export function handler(event, context, callback) {
 
   var username = event.path.replace("/get-avatar","");
 
+  console.log("Username: ", username);
+
   request('https://mobile.twitter.com/' + username, function(err, response, body){
 
     // format the response to be a bit mor concise and return it to the client
     if(!err && response.statusCode === 200){
-
       const $ = cheerio.load(body);
-
       const avatar = ($('.avatar img').attr('src') || '').replace('_normal', '_400x400');
-
       return callback(null, {
           statusCode: 200,
           headers: {"Content-Type": "application/json"},
@@ -23,10 +22,10 @@ export function handler(event, context, callback) {
         })
       } else {
         return callback(null, {
-          statusCode: 200,
+          statusCode: 404,
           body: err
         })
       }
-    });
+    })
 
 }
