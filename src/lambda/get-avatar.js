0d0267be0a10a82a9f9b09645671bf315bac4301
Phil Hawksworth <phil@netlify.com>
2018-05-04 18:08:54 +0100

    Update get-avatar.js

--- src/lambda/get-avatar.js
+++ src/lambda/get-avatar.js
@@ -6,13 +6,10 @@ const request = require('request');
 export function handler(event, context, callback) {
 
   // grab the parameters from the request
-//   var size = event.queryStringParameters['size'];
-//   var user = event.queryStringParameters['user'];
-  
   const { size, user } = event.queryStringParameters;
 
   // transform the size parameter into the image URL pattern needed in the request
-  var option = {
+  const option = {
     "small" : "_normal",
     "large" : "_400x400"
   };
